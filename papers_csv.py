0d258c41c7a00ed410018450042fda8667e55c5d
Milovan Suvakov <suvakov@gmail.com>
2020-03-31 17:27:41 -0500

    Updating paper script

--- papers_csv.py
+++ papers_csv.py
@@ -4,30 +4,83 @@ import argparse
 import os
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from time import sleep, localtime, strftime
 import re
 import sys
 from collections import defaultdict
 
 class MyNCBI:
-    myncbi = webdriver.Chrome(); # or webdriver.PhantomJS(service_log_path=os.path.devnull)
-    myncbi.implicitly_wait(5)
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    path = 'chromedriver'
 
-    pubmed = webdriver.Chrome(); # or webdriver.PhantomJS(service_log_path=os.path.devnull)
-    pubmed.implicitly_wait(5)
+    def __init__(self, authid, members_fname=None):
+        self.papers = defaultdict(list)
+        self.set_members(members_fname)
 
-    members = None
-    papers = defaultdict(list)
+        self.myncbi = webdriver.Chrome(executable_path=self.path, options=self.options)
+        self.myncbi.implicitly_wait(5)
+        self.pubmed = webdriver.Chrome(executable_path=self.path, options=self.options)
+        self.pubmed.implicitly_wait(5)
 
-    def __init__(self, cid):
-        self.scrap_myncbi(cid)
+        self.scrap_myncbi(authid)
 
-    def scrap_myncbi(self, cid):
-        self.myncbi.get("http://www.ncbi.nlm.nih.gov/sites/myncbi/browse/collection/{cid}/?sort=date&direction=ascending".format(cid=cid))
-#        self.myncbi.get("https://www.ncbi.nlm.nih.gov/myncbi/alexej.abyzov.1/bibliography/public/")
+        self.myncbi.quit()
+        self.pubmed.quit()
+
+    def scrap_myncbi(self, authid):
+        self.myncbi.get("https://www.ncbi.nlm.nih.gov/myncbi/{authid}.1/bibliography/public/?sortby=pubDate&sdirection=ascending".format(authid=authid))
+        index = 0
+        while True:
+            for docsum in self.myncbi.find_elements_by_xpath('//div[@class="ncbi-docsum"]'):
+                index += 1
+
+                try:
+                    title = docsum.find_element_by_xpath('./span[@class="title"]').text
+                except NoSuchElementException:
+                    title = docsum.find_element_by_xpath('./a').text
+                
+                sys.stderr.write("Scraping {index}. {title}...\n".format(index=index, title=title[:40]))
+                
+                try:
+                    pmid = docsum.find_element_by_xpath('./span[@class="pmid"]').text.split()[-1]
+                    year, paper = self.paper_from_pmid(pmid)
+                    paper = paper.format(index=index, title=title)
+                except NoSuchElementException:
+                    author = docsum.find_element_by_xpath('./span[@class="authors"]').text.rstrip(".")
+                    author = self.highlight_members(author)
+                    year = docsum.find_element_by_xpath('./span[@class="displaydate"]').text[:4]
+                    page = docsum.find_element_by_xpath('./span[@class="page"]').text.rstrip(".")
+                    try:
+                        journal = docsum.find_element_by_xpath('./span[@class="journalname"]').text.rstrip(".")
+                        volume = docsum.find_element_by_xpath('./span[@class="volume"]').text
+                        issue = docsum.find_element_by_xpath('./span[@class="issue"]').text
+                        
+                        paper = '''{title}|{author}|{journal}|{year}|{volume} {issue}; {page}||||\n'''.format(index=index, title=title, 
+                                        author=author, year=year, journal=journal, issue=issue, volume=volume, page=page)
+
+                        
+                    except NoSuchElementException:    
+                        editor = docsum.find_element_by_xpath('./span[@class="editors"]').text    
+                        ch_num = docsum.find_element_by_xpath('./span[@class="chapter-details"]').text
+                        ch_title = docsum.find_element_by_xpath('./span[@class="chaptertitle"]').text
+                        publisher = docsum.find_element_by_xpath('./span[@class="book-publisher"]').text
+                        
+                        paper = '''{title}|{author}|{chapter}|{year}|{issue}; {page}||||\n'''.format(index=index, chapter=ch_title+", "+publisher, title=title, 
+                                          author=author, issue=ch_num, year=year, page=page) 
+                        
+                self.papers[year].append(paper)
+
+            try:
+                self.myncbi.find_element_by_xpath('//a[@class="nextPage enabled"]').click()
+            except NoSuchElementException:
+                break
 
     def set_members(self, fname):
-        if fname is not None:
+        if fname is None:
+            self.members = None
+        else:
             with open(fname) as f:
                 self.members = [line.strip() for line in f]
 
@@ -51,66 +104,19 @@ class MyNCBI:
         except IndexError:
             issue = "[Epub ahead of print]"
         paper = '{{title}}|{author}|{journal}|{year}|{issue}|'.format(author=author, journal=journal, year=year, issue=issue)
-
+        
         link_tag = self.pubmed.find_elements_by_xpath('//div[@class="icons portlet"]/a')
         if link_tag:
             paper += '{href}|{src}|'.format(href=link_tag[0].get_attribute('href'),
               src=link_tag[0].find_element_by_tag_name('img').get_attribute('src'))
         else:
-          paper += '||'
+            paper += '||'
         paper += 'https://ncbi.nlm.nih.gov/pubmed/{pmid}|imgs/pubmed.png\n'.format(pmid=pmid)
 
         return (year, paper)
 
-    def parse_myncbi(self):
-        while True:
-            title_tags = self.myncbi.find_elements_by_xpath('//div[@class="rprt"]')
-
-            for tag in title_tags:
-                text = tag.text.split('\n')
-                index = text[0][:-1]
-                title = text[1]
-                sys.stderr.write("Parsing {index}. {title}...\n".format(
-                        index=index, title=" ".join(title.split()[:4])))
-
-                source = text[-1]
-                pub_type = re.search("\[(.+)\]", source).group(1)
-                if pub_type == "book":
-                    author = self.highlight_members(text[2].rstrip("."))
-                    issue, info = text[3].split("; ")
-                    issue = issue.rstrip(".")
-                    year = info[:4]
-                    chapter, page = re.search("(Chapter.+)(\d+-\d+)p.", info).groups()
-                    paper = '''{title}|{author}|{chapter}|{year}|{issue}; {page}||||\n'''.format(index=index, chapter=chapter, title=title, 
-                                          author=author, issue=issue, year=year, page=page) 
-                elif pub_type == "journal": 
-                    m = re.search("PMID: (\d+)", source)
-                    if m:
-                        pmid = m.group(1)
-                        year, paper = self.paper_from_pmid(pmid)
-                        paper = paper.format(index=index, title=title)
-                    else:
-                        author, info = text[2:4]
-                        author = self.highlight_members(author.rstrip("."))
-                        journal, issue = info.split(". ")[:2]
-                        issue = issue.rstrip(".")
-                        year = issue[:4]
-                        paper = '''{title}|{author}|{journal}|{year}|{issue}||||\n'''.format(index=index, title=title, author=author, year=year, journal=journal, issue=issue)
-                self.papers[year].append(paper)
-
-            page_input_tag = self.myncbi.find_element_by_xpath('//input[@class="num"]')
-            last = page_input_tag.get_attribute('last')
-            page = page_input_tag.get_attribute('value')
-
-            if page == last: 
-                break
-
-            next_tag = self.myncbi.find_element_by_link_text('Next >')
-            next_tag.click()
-
-    def get_csv(self):
-        self.parse_myncbi()
-
+    @property
+    def csv(self):
         total_n = 0
         csv = ""
         for year, year_papers in sorted(self.papers.items(), reverse=True):
@@ -125,13 +131,13 @@ def main():
         description='html builder for a publication list')
 
     parser.add_argument('-m', '--members', metavar='lab_member_list.txt', help='Lab member list to highlight in authors')
-    parser.add_argument('-c', '--cid', metavar='40799671', required=True, help='My NCBI collection number')
+    parser.add_argument('-a', '--authid', metavar='firstname.lastname', required=True, help='My NCBI author name. ex) alexej.abyzov')
 
     args = parser.parse_args()
     
-    m = MyNCBI(args.cid)
-    m.set_members(args.members)
-    print(m.get_csv(),end='')
+    m = MyNCBI(args.authid, args.members)
+    print(m.csv,end='')
 
 if __name__ == "__main__":
     main()
+    
